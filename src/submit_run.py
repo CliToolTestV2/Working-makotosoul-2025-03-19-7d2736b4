3e224fc64e60920a14f8ad312c58ca8c0383b637
makotosoul <148573685+makotosoul@users.noreply.github.com>
2025-03-19 16:46:20 +0200

    Update submit_run.py

--- src/submit_run.py
+++ src/submit_run.py
@@ -5,10 +5,11 @@ import sys
 sys.path.append('../src')
 from pipelines.pipeline_definitions.pipeline_definition import pipeline
 from pipelines.pipeline_arg.pipeline_arg import arguments
+KFP_ENDPOINT = "http://localhost:8080"
 
 def submit_pipeline():
 
-    client = kfp.Client() 
+    client = kfp.Client(host=KFP_ENDPOINT) 
     
 
     # Define your experiment and run name
